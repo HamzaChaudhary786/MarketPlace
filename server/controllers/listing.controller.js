0522c5704c8ea511076505b27918a8e358826a4c
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-09-02 08:32:26 -0700

    add delete listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,10 +1,38 @@
-import Listing  from "../models/listing.model.js"
+import Listing from "../models/listing.model.js"
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
-    try {
-      const listing = await Listing.create(req.body);
-      return res.status(201).json(listing);
-    } catch (error) {
-      next(error);
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+
+export const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+      return res.status(404).json({ message: "Listing not found" });
+
     }
-  };
+
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, "You can change own listing"))
+    }
+
+
+
+    await Listing.findByIdAndDelete(req.params.id)
+    res.status(200).json({ message: "Listing has been deleted" });
+
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.controller.js';
+import { createListing , deleteListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -7,6 +7,8 @@ const router = express.Router();
 
 
 router.post ('/create',verifyToken ,createListing)
+
+router.delete ('/delete/:id',verifyToken ,deleteListing)
 
 
 export default router;

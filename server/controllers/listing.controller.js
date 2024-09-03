1cb240cd2db0667cbfbfad452c5e0ab47d8693a5
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-09-03 00:55:05 -0700

    create update the listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -14,22 +14,61 @@ export const createListing = async (req, res, next) => {
 
 
 export const deleteListing = async (req, res, next) => {
+
+
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return res.status(404).json({ message: "Listing not found" });
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can change own listing"));
+  }
+
+
   try {
-    const listing = await Listing.findById(req.params.id);
-
-    if (!listing) {
-      return res.status(404).json({ message: "Listing not found" });
-    }
-
-    if (req.user.id !== listing.userRef) {
-      return next(errorHandler(401, "You can change own listing"));
-    }
-
     await Listing.findByIdAndDelete(req.params.id);
-    
+
+    return res.status(200).json({ message: "Listing has been deleted" });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+
+
+
+export const updateListing = async (req, res, next) => {
+
+
+  const listing = await Listing.findById(req.params.id);
+
+
+  if (!listing) {
+    return res.status(404).json({ message: "Listing not found" });
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can change own listing"));
+  }
+
+  try {
+    const upadteListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(upadteListing);
+
+
     // Send the deletion success message
     return res.status(200).json({ message: "Listing has been deleted" });
-    
+
   } catch (error) {
     next(error);
   }
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing , deleteListing } from '../controllers/listing.controller.js';
+import { createListing , deleteListing , updateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,6 +9,6 @@ const router = express.Router();
 router.post ('/create',verifyToken ,createListing)
 
 router.delete ('/delete/:id',verifyToken ,deleteListing)
-
+router.post('/update/:id',verifyToken ,updateListing)
 
 export default router;

0ba270104b6f5dcd6ed5869bf89b814f9c36c150
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-09-06 20:43:42 -0700

    resolve path issue in index file

--- server/index.js
+++ server/index.js
@@ -39,13 +39,11 @@ app.use('/api/listing', listingRouter);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
 
